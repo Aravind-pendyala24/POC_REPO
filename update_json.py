2aaf11e5877f02e687784d75b0b516085d442b0d
pendyala <pendyalaaravind@gmail.com>
2024-08-28 22:55:19 +0530

    Added files

---
+++ update_json.py
@@ -0,0 +1,23 @@
+import yaml
+import json
+
+YAML_FILE = 'D:/PYTHON/POC/config2.yaml'
+JSON_FILE= 'D:/PYTHON/POC/test.json'
+
+# Read the full YAML file
+with open( YAML_FILE, 'r') as yaml_file:
+    yaml_content = yaml.safe_load(yaml_file)
+
+# Extract the 'uat' section
+uat_content = yaml_content.get('uat', {})
+
+# Read the existing JSON content
+with open( JSON_FILE, 'r') as json_file:
+    json_content = json.load(json_file)
+
+# Update the JSON content with the extracted 'uat' section
+json_content.update(uat_content)
+
+# Write the updated content back to the JSON file
+with open(JSON_FILE, 'w') as json_file:
+    json.dump(json_content, json_file, indent=4)
