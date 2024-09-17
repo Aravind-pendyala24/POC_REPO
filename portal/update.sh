56fd7bdf35372f7e6f4f403184511c22493a8544
Aravind Pendyala <80102707+Aravind-pendyala24@users.noreply.github.com>
2024-09-17 15:07:53 +0530

    Update update.sh

--- portal/update.sh
+++ portal/update.sh
@@ -10,12 +10,12 @@ export TZ="America/Chicago"
 
 formatted_date=$(date "+%m/%d/%Y %H:%M %Z")
 
-#lock_file="/poc/scripts/xml/list.lock"
+lock_file="/poc/scripts/xml/list.lock"
 
-#flock -n "$lock_file" -c "
+flock -n "$lock_file" -c "
        awk -i inplace -v portal_code=\"${portal_code}\" -v new_tag_txt=\"${new_tag_txt}\" '{if (\$0 ~ portal_code) gsub(/tag=\"[^\"]*\"/, \"tag=\\\"\"new_tag_txt \"\\\"\"); print}' \"${file_name}\"
 
        awk -i inplace -v portal_code=\"${portal_code}\" -v date_text=\"${formatted_date}\" '{if (\$0 ~ portal_code) gsub(/ldeploy=\"[^\"]*\"/, \"ldeploy=\\\"\"date_text \"\\\"\"); print}' \"${file_name}\"
-#"
+"
 
-#rm -f "$lock_file"
+rm -f "$lock_file"
