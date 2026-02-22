95f0eeb928df17f6aef15a6d62ee360a2fe0c273
Aravind Pendyala <80102707+Aravind-pendyala24@users.noreply.github.com>
2026-02-23 00:26:01 +0530

    Update db_migration.tf

--- db-iac/db_migration.tf
+++ db-iac/db_migration.tf
@@ -21,6 +21,7 @@ resource "null_resource" "db_migration" {
   }
 }
 
+#v2
 resource "null_resource" "db_migration" {
 
   for_each = var.execute_migration ? { run = true } : {}
@@ -40,5 +41,34 @@ resource "null_resource" "db_migration" {
         ${var.db_password} \
         ${path.module}/../sql/scripts/${var.sql_file}
     EOT
+  }
+}
+
+#v3
+resource "null_resource" "db_migration" {
+
+  count = var.execute_migration ? 1 : 0
+
+  triggers = {
+    sql_files = join("|", var.sql_files)
+  }
+
+  provisioner "local-exec" {
+
+    command = "python3 ${path.module}/../scripts/run_migration.py"
+
+    environment = {
+      ENVIRONMENT   = var.environment
+      DB_HOST       = module.postgres.cluster_endpoint
+      DB_PORT       = "5432"
+      DB_NAME       = var.db_name
+      DB_USER       = var.db_username
+      DB_PASSWORD   = local.db_secret.password
+
+      # Simple delimiter string
+      SQL_FILES     = join("|", var.sql_files)
+
+      SQL_BASE_PATH = "${path.module}/../sql/scripts"
+    }
   }
 }
